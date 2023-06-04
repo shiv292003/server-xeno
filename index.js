3025364376157b633bff395aaa59dff6ff422d55
shiv292003 <90518426+shiv292003@users.noreply.github.com>
2023-06-05 00:00:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,177 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to MongoDB:", err);
+});
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if the username is already taken
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.status(200).json({ message: "Registration successful" });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if the user exists
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    // Compare the password with the stored hash
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    // Generate a JWT token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+const contactSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+});
+
+const Contact = mongoose.model("Contact", contactSchema);
+// Middleware to verify the JWT token
+const authenticateToken = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  console.log(token);
+
+  //    console.log("token");
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Forbidden" });
+    }
+
+    req.userId = user.userId;
+    next();
+  });
+};
+
+app.post("/api/contacts", authenticateToken, async (req, res) => {
+  try {
+    const { name, email, phoneNumber } = req.body;
+
+    const newContact = new Contact({
+      userId: req.userId,
+      name,
+      email,
+      phoneNumber,
+    });
+    await newContact.save();
+
+    res.status(200).json(newContact);
+  } catch (error) {
+    console.error("Error creating contact:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/api/contacts", authenticateToken, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ userId: req.userId });
+    res.status(200).json(contacts);
+  } catch (error) {
+    console.error("Error retrieving contacts:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.put("/api/contacts/:id", authenticateToken, async (req, res) => {
+  try {
+    const { name, email, phoneNumber } = req.body;
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { name, email, phoneNumber },
+      { new: true }
+    );
+    if (!updatedContact) {
+      return res.status(404).json({ error: "Contact not found" });
+    }
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    console.error("Error updating contact:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.delete("/api/contacts/:id", authenticateToken, async (req, res) => {
+  try {
+    const deletedContact = await Contact.findByIdAndDelete(req.params.id);
+
+    if (!deletedContact) {
+      return res.status(404).json({ error: "Contact not found" });
+    }
+
+    res.status(200).json({ message: "Contact deleted" });
+  } catch (error) {
+    console.error("Error deleting contact:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
